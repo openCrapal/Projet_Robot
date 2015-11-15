f524dfc406da4936b9e3ecdd8e7f60aa07b1260c
ulysse <ulysse.delplanque@gmail.com>
2015-11-15 21:44:39 +0100

    now use an instance of class i2cDevice to control grove i2c motor driver

--- Python/i2cBus/grove_motor.py
+++ Python/i2cBus/grove_motor.py
@@ -4,7 +4,7 @@
 import smbus
 import math
 import time
-import subprocess
+from i2cFunctions import *
 
 # default adress of the grove motor controller
 motor_adress = 0x0f
@@ -14,37 +14,19 @@ set_freq = 0x84
 motor_1 = 0xa1
 motor_2 = 0xa5
 
-bus = smbus.SMBus(1) # or bus = smbus.SMBus(1) for Revision 2 boards
+Grove_motor = I2cDevice(motor_adress)
 
-def read_byte(adr):
-    return bus.read_byte_data(motor_adress, adr)
-
-def read_word(adr):
-    high = bus.read_byte_data(motor_adress, adr)
-    low = bus.read_byte_data(motor_adress, adr+1)
-    val = (high << 8) + low
-    return val
-
-def read_word_2c(adr):
-    val = read_word(adr)
-    if (val >= 0x8000):
-        return -((65535 - val) + 1)
-    else:
-        return val
-
-
-
-def init():
-	# to begin, set the pwm frequency
-	try:
-		bus.write_byte_data(motor_adress, set_freq, 2)
-	except IOError:
-		print(" Error connecting to device at the adress: ", motor_adress)
-		subprocess.call(['i2cdetect', '-y', '1'])
-		flag = 1     #optional flag to signal your code to resend or something
-		pass
+# to begin, set the pwm frequency
+try:
+	Grove_motor.write_byte(set_freq, 4)
+except IOError:
+	print(" Error connecting to motor at the adress: ", motor_adress)
+	#subprocess.call(['i2cdetect', '-y', '1'])
+	#flag = 1     #optional flag to signal your code to resend or something
+	#pass
 
 #Test of the module
 if __name__ == "__main__":
-	init()
+	Grove_motor.write_byte(0xa1, -50)
+	
 	
