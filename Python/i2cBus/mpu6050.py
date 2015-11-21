3c40040ba5fabb069398ad09ddef78617a91946b
ulysse@pi <ulysse.delplanque@gmail.com>
2015-11-21 15:44:24 +0100

    mpu6050 seems to operate ok. Still, the z channel I'm using is a bit noisy

--- Python/i2cBus/mpu6050.py
+++ Python/i2cBus/mpu6050.py
@@ -8,6 +8,12 @@ import math
 # Power management registers
 power_mgmt_1 = 0x6b
 power_mgmt_2 = 0x6c
+set_scale = 0x1b
+sample_rate = 0x19
+
+bus = smbus.SMBus(1) # or bus = smbus.SMBus(1) for Revision 2 boards
+address = 0x68       # This is the address value read via the i2cdetect command
+
 
 offset_z = 0.0
 
@@ -34,7 +40,7 @@ def get_gyro_z():
 
 def save_offset():
 	i = 0
-	n = 100.0
+	n = 500.0
 	sum = 0.0
 	while(i<n):
 		sum = sum + read_word_2c(0x47)
@@ -44,48 +50,24 @@ def save_offset():
 	return sum
 
 def init():
+	global offset_z
 	# Wake the 6050 up as it starts in sleep mode
 	bus.write_byte_data(address, power_mgmt_1, 0)
+	bus.write_byte_data(address, set_scale, 2)   # 2->+-1000grad/sec
+	bus.write_byte_data(address, sample_rate, 7)   # 7->1kHz min value, already quite enought
 	offset_z = save_offset()
 
-bus = smbus.SMBus(1) # or bus = smbus.SMBus(1) for Revision 2 boards
-address = 0x68       # This is the address value read via the i2cdetect command
 
 # Test of the module
 if __name__ == "__main__":
 	init()
-	print ("gyro data")
-
-	gyro_xout = read_word_2c(0x43)
-	gyro_yout = read_word_2c(0x45)
-	#gyro_zout = read_word_2c(0x47)
-	gyro_zout = get_gyro_z()
-
-	print ("gyro_xout: ", gyro_xout, " scaled: ", (gyro_xout / 131))
-	print ("gyro_yout: ", gyro_yout, " scaled: ", (gyro_yout / 131))
-	print ("gyro_zout: ", gyro_zout, " scaled: ", (gyro_zout / 131))
-
-	print ("\n accelerometer data")
-
-	accel_xout = read_word_2c(0x3b)
-	accel_yout = read_word_2c(0x3d)
-	accel_zout = read_word_2c(0x3f)
-
-	accel_xout_scaled = accel_xout / 16384.0
-	accel_yout_scaled = accel_yout / 16384.0
-	accel_zout_scaled = accel_zout / 16384.0
-
-	print ("accel_xout: ", accel_xout, " scaled: ", accel_xout_scaled)
-	print ("accel_yout: ", accel_yout, " scaled: ", accel_yout_scaled)
-	print ("accel_zout: ", accel_zout, " scaled: ", accel_zout_scaled)
-
 	j = 0
 	sum = 0.0
-	while(j<10):
+	while(j<10000):
 		val = get_gyro_z()
 		
 		print ("z centre :" , val)
 		j = j + 1
 		sum += val
-	sum /= 10.0
+	sum /= 10000.0
 	print ("moyenne : ", sum)
