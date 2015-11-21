8da1b9de07a662b12a91dc4cb111f139e3acbf6c
ulysse@pi <ulysse.delplanque@gmail.com>
2015-11-22 00:48:22 +0100

    Pretty cool tools for automation. PID not tested yet

--- Python/automation.py
+++ Python/automation.py
@@ -15,18 +15,20 @@ class Z_Constant:
 
 	def _update(self):
 		# well, if this is a constante, bet you'll do nothing
+		# print ("Z_Constante._update")
 		pass
+
 
 	def get_val(self):
 		global Z_Index
 		if (self._local_index < Z_Index):
-			self.update()
+			self._update()
 			self._timer0 = time.time()
 			self._local_index = Z_Index
 		return self._valeur
 	
 	# use with responsability
-	def set_val(n_val):
+	def set_val(self, n_val):
 		self._valeur = n_val
 
 
@@ -39,11 +41,13 @@ class Z_Filter(Z_Constant):
 				raise TypeError("Not a instance of Z_Constante")
 		except:
 			print("Invalid argument, objet of type Z_ required")
-		self.Z_Item = Z_item  # reference to an objet wich inherits from Z_Constant
+		self._Z_Item = Z_item  # reference to an objet wich inherits from Z_Constant
 		self._memorie = 0.0
 		try:
 			if band_pass > 0:
 				self._t_cut = band_pass
+			else:
+				raise ValueError("Filter band_pass time must be > 0")
 		except:
 			print("Invalid argument, positive band_pass value")
 
@@ -52,16 +56,76 @@ class Z_Filter(Z_Constant):
 		 # first order linear filter
 		val = self._Z_Item.get_val()
 		t = time.time()
+		#print ("Z_filter.update, val: ", val )
+		self._memorie += val * ( t - self._timer0) / self._t_cut
+		self._valeur = self._memorie
 
-		self.memorie += (t-self._timer0) / self._t_cut  * val
+
+class Z_Derivative(Z_Filter):
+	# Oh yeah, speed = Z_Derivative( my_Z_position ) Awsome right?
+	def __init__(self, Z_item):
+		Z_Filter.__init__(self, Z_item)
+		
+	def _update(self):
+		self._valeur = (self._Z_Item.get_val() - self._memorie) / ( time.time() - self._timer0)
+		self._memorie = self._Z_Item.get_val()
+
+class Z_PID(Z_Filter):
+	# Once you've use a PID controller, it all seems fairly natural. Otherways, it's kind of complicated
+	# I propose hier a state of the art PID algorithm with posibility of speed pre-control surch as thoses
+	# used in actuals industrial robots, mills... You can still improve by adding a band-pass but that's kind of fancy
+	# You want to control a position, a temperature, a force?
+	# This algorithm compares the value you want, the value from the capture (and their derivatives),
+	# and decide the adapted electrical voltage (or whaterver energy you use) for you actuator
+	def __init(self, kv, kp, tp, sat, Z_Goal, Z_Sensor = Z_Constant(0.0), Z_D_Goal = Z_Constant(0.), Z_D_Sensor = Z_Constant(0.)):
+		try:	
+			if (not isinstance(Z_Goal, Z_Constant) and not isinstance(Z_Sensor, Z_Constant) and not isinstance(Z_D_Goal, Z_Constant) and not isinstance(Z_D_Sensor, Z_Constant)):
+				raise TypeError("Not a instance of Z_Constante")
+		except:
+			print("Invalid argument, objet of type Z_ required")
+		Z.Filter.__init__(self, Z_Goal)
+		self._memorie = 0.0
+		self._ecart_old = 0.0
+		self._Sensor = Z_Sensor
+		self._D_Sensor = Z_D_Sensor
+		self._D_Goal = Z_D_Goal
+		self._kv = kv
+		self._kp = kp
+		if not tp > 0:
+			self._tp = 1.0
+		else:
+			self._tp = tp
+		if not sat > 0:
+			self._sat = 1.0
+		else:
+			self._sat = sat
+
+	def update(self):
+		dt = time.time() - self._timer0
+		ecart = self._kv * (self._Z_Item.get_val() - self._Sensor.get_val())
+		ecart += self._D_Goal.get_val() - self._D_Sensor.get_val()
+		self._memorie += ecart * dt / self._tp
+		self._valeur = ecart + self._memorie * self._kp + (ecart - self._ecart_old) * self._kd / dt
+		self._ecart_old = ecart
+		
+		# Intelligent saturation : the Integral part (memorie) doesn't keep uselessly growing
+		if self._valeur > self._sat:
+			self._valeur = self._sat
+			self._memorie = self.sat - ecart * self._kp
+		elif self._valeur < -self._sat:
+			self._valeur = - self._sat
+			self._memorie = - self._sat - self._kp * ecart
+
 		
 #test du module
 if __name__ == "__main__":
 	Zero = Z_Constant(0)
 	Filtre = Z_Filter(Zero, 0.2)
-	for i in range(1, 1000, 1):
+	Deriv = Z_Derivative(Filtre)
+	for i in range(1, 200, 1):
 		Z_Index += 1
-		Zero.set_val(math.sin(i/10))
-		print ("val: ", Zero.get_val(), "\t filtre: ", Filtre.get_val())
+		ang = math.sin(i/10)
+		Zero.set_val(ang)
+		print ("val: ", Zero.get_val(), "\t filtre: ", Filtre.get_val(), " \t Derivee: ", Deriv.get_val())
 		time.sleep(0.01)
 		
