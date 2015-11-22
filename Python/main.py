9952c0406838d66657b143eebeec1f3d95776a6b
ulysse@pi <ulysse.delplanque@gmail.com>
2015-11-22 15:41:24 +0100

    I begin working on the main programm

--- Python/main.py
+++ Python/main.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3.4
 # -*-coding:utf-8 -*
 
-import i2cBus.grove_motor as i2c
-import i2cBus.mpu6050 as i2c
+import time
+import mpu6050
+mpu6050.init()
+import localisation
+localisation.init()
 
-i2c.Grove_motor.write_byte(0xa1, -50)
-
-#set_freq = 0x84
-# motor_1 = 0xa1
-# motor_2 = 0xa5
-
+time.sleep(5)
 
