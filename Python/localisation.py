08023a117041afe0054515c204fd8920755ddfd2
ulysse@pi <ulysse.delplanque@gmail.com>
2015-11-23 22:48:18 +0100

    use new improved Encoder class for localisation

--- Python/localisation.py
+++ Python/localisation.py
@@ -5,24 +5,23 @@ import RPi.GPIO as GPIO
 import math
 from time import time, sleep
 
+#GPIO.cleanup()
+GPIO.setmode(GPIO.BCM) # use hardware pin numbers
+GPIO.setwarnings(True)
+
+#Pins I use
 pinLeftA = 14
 pinLeftB = 15
 pinRightA= 18
 pinRightB= 23
 
-R_A_old = 0
-R_B_old = 0
-L_A_old= 0
-L_B_old= 0
-
+#Global localisition variables
 pos_x = 0.0
 pos_y = 0.0
 pos_teta = 0.0    #orientation in rads
 pos_speed = 0.0   #axial speed
 
 pos_errors = 0
-count_l = 0
-count_r = 0
 timer = time()
 
 # physical dimentions of the car
@@ -30,69 +29,86 @@ dxLeft = 0.042*3.141/48
 dxRight= 0.042*3.141/48
 wheels_width = 0.128   # distance beetwin wheels
 
-#GPIO.cleanup()
-GPIO.setmode(GPIO.BCM) # use hardware pin numbers
-GPIO.setwarnings(True)
+class Encoder():
 
-GPIO.setup(pinLeftA,  GPIO.IN, pull_up_down=GPIO.PUD_UP)  
-GPIO.setup(pinLeftB,  GPIO.IN, pull_up_down=GPIO.PUD_UP)  
-GPIO.setup(pinRightA, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
-GPIO.setup(pinRightB, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+	def changeA(self, term):
+		newA = self.INPUT(self.pinA)
+		if self.A == newA:
+			self.erreur +=1
+		elif newA:  # rising edge on A
+			if self.B:
+				self.count -= 1
+			else:
+				self.count += 1
+		else:
+			if self.B:
+				self.count += 1
+			else:
+				self.count -= 1
+		self.A = newA
 
-L_A_old = GPIO.input(pinLeftA)
-L_B_old = GPIO.input(pinLeftB)
-R_A_old= GPIO.input(pinRightA)
-R_B_old= GPIO.input(pinRightB)
+	def changeB(self, term):
+		newB = self.INPUT(self.pinB)
+		if newB == self.B:
+			self.erreur += 1
+		elif newB:  # rising edge on B
+			if self.A:
+				self.count += 1
+			else:
+				self.count -= 1
+		else:
+			if self.A:
+				self.count -= 1
+			else:
+				self.count += 1
+		self.B = newB
 
-# Define encoder count function
-def leftEncoder(term):
-	global L_B_old, L_A_old, pos_errors, count_l
-	
-	A = GPIO.input(pinLeftA)  # stores the value of the encoders at time of interrupt
-	B = GPIO.input(pinLeftB)
-
-	if (not A and not B and not L_A_old and L_B_old or not A and B and L_A_old and L_B_old or A and B and L_A_old and not L_B_old or A and not B and not L_A_old and not L_B_old):
-		# this will be clockwise rotation
-		count_l += 1
-
-
-	elif (not A and B and not L_A_old and not L_B_old or A and B and not L_A_old and L_B_old or A and not B and L_A_old and L_B_old or not A and not B and L_A_old and not L_B_old):
-		# this will be counter-clockwise rotation
-		count_l -= 1
-		#print 'Encoder count is %s' %counts
-		#print 'AB is %s %s' % (Encoder_A, Encoder_B)
-
-
-	else:
-	#this will be an error
-		pos_errors += 1
-	
-	L_A_old = A
-	L_B_old = B # store the current encoder values as old values to be used as comparison in the next loop
-	
-
-def rightEncoder(term):
-	global  R_B_old, R_A_old, pos_errors, count_r
-	A = GPIO.input(pinRightA)  # stores the value of the encoders at time of interrupt
-	B = GPIO.input(pinRightB)
-
-	if (not A and not B and not R_A_old and R_B_old or not A and B and R_A_old and R_B_old or A and B and R_A_old and not R_B_old or A and not B and not R_A_old and not R_B_old):
-		#this will be counter-clockwise rotation
-		count_r -= 1
-		#print 'Encoder count is %s' %counts
+	def fallB(self, term):
+		if self.A:
+			self.count -= 1
+		else:
+			self.count += 1
+		self.B = False
 
 
-	elif (not A and B and not R_A_old and not R_B_old or A and B and not R_A_old and R_B_old or A and not B and R_A_old and R_B_old or not A and not B and R_A_old and not R_B_old):
-		# this will be clockwise rotation
-		count_r += 1
+	def __init__(self, pinA, pinB):
+		self.INPUT = GPIO.input
+		self.pinA = pinA
+		self.pinB = pinB
+		self.count = 0
+		self.erreur= 0
+		GPIO.setup(pinA, GPIO.IN, pull_up_down=GPIO.PUD_UP) # pull_up_down=GPIO.PUD_UP
+		GPIO.setup(pinB, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+		self.A = GPIO.input(pinA)
+		self.B = GPIO.input(pinA)
+		done = False
+		while not done:
+			try:
+				GPIO.add_event_detect(pinA, GPIO.BOTH, callback = self.changeA) 
+			except:
+				pass
+			else:
+				done = True
+		done = False
+		while not done:
+			try:
+				GPIO.add_event_detect(pinB, GPIO.BOTH, callback = self.changeB ) 
+			except:
+				pass
+			else:
+				done = True
 
-	else:
-        #this will be an error
-		pos_errors += 1
-	
-	R_A_old = A
-	R_B_old = B
-	
+	def get_count(self):
+		c = self.count
+		self.count = 0
+		return c
+
+# Declaration of the two object "Encoders" used of localisation
+Enc_Left = Encoder(pinLeftA, pinLeftB)
+Enc_Right= Encoder(pinRightA,pinRightB)
+
+
+# I must have done too much c++, there must be a more pythonic way to do that
 def get_teta():
 	# update()
 	return (pos_teta)
@@ -106,14 +122,15 @@ def get_x():
 def get_y():
 	return pos_y
 
-
+# To save time during interruptions, the "complex" trigonometric calculation is
+# only done when the user need it
+# if you don't call it often enought, you might have some big localisation errors
 def update():
 	global timer
 	t = time()
-	if (t - timer < 0.001):
-			return
-
-	global pos_x, pos_y, pos_teta, pos_speed, count_l, count_r
+	global pos_x, pos_y, pos_teta, pos_speed
+	count_l = Enc_Left.get_count()
+	count_r = Enc_Right.get_count()
 	d_way = (count_l * dxLeft + count_r * dxRight) / 2.0
 	pos_speed = d_way / ( t - timer )
 	half_d_teta  = count_l * math.atan(dxLeft/2.0/wheels_width) - count_r * math.atan(dxRight/2.0/wheels_width)
@@ -122,45 +139,7 @@ def update():
 	pos_y += d_way * math.sin(pos_teta)
 	pos_teta -= half_d_teta
 	timer = t
-	count_l = count_r = 0
-
-# Initialize the interrupts - these trigger on the both the rising and falling 
-done = False
-while (not done):
-	try:
-		GPIO.add_event_detect(pinRightA, GPIO.BOTH, callback = rightEncoder)   # Encoder A
-	except:
-		pass
-	else:
-		done = True
-
-done = False
-while (not done):
-	try:
-		GPIO.add_event_detect(pinRightB, GPIO.BOTH, callback = rightEncoder)   # Encoder B
-	except:
-		pass
-	else:
-		done = True
-
-done = False
-while (not done):
-	try:
-		GPIO.add_event_detect(pinLeftA, GPIO.BOTH, callback = leftEncoder)   # Encoder A
-	except:
-		pass
-	else:
-		done = True
-
-done = False
-while (not done):
-	try:
-		GPIO.add_event_detect(pinLeftB, GPIO.BOTH, callback = leftEncoder)   # Encoder B
-	except:
-		pass
-	else:
-		done = True
-
+	pos_errors = Enc_Right.erreur + Enc_Left.erreur
 
 if __name__ == "__main__":
 	import sys
@@ -175,9 +154,9 @@ if __name__ == "__main__":
 
 	i = 0
 	print ("pos_x \t pos_Y \t pos_teta \t pos_speed")
-	while(i<100):
+	while(i<200):
 		update()
-		print (pos_x, pos_y, pos_teta, pos_speed)
+		print (pos_x, pos_y, pos_teta, pos_speed, pos_errors)
 		i += 1
-		sleep(0.2)
+		sleep(0.01)
 
--- Python/main.py
+++ Python/main.py
@@ -10,11 +10,19 @@ import automation as Z
 import sys
 import signal
 
-kva = 25.
-kpa = 0.01 #  0.005
-tpa = .2   #  4.0
-kda = 0.00001 # 0.00001
-sata= 100.0
+# PID inclinaison
+kva = 0.0  #  25
+kpa = 0.01 #  0.01
+tpa = 1000000.1   #  0.2
+kda = 0.0000 # 0.00001
+sata= 50.0   # 100
+
+# PID orientation
+kvo = 0.5
+kpo = 50 # 50
+tpo = 0.0001   #  0.0001
+kdo = 0.00# 0.00001
+sato= 30.0
 
 def fermer_pgrm(signal, frame):
 	print("fermer proprement")
@@ -29,22 +37,24 @@ Goal = Z.Z_Constant(10.0)
 D_Gyro = Z.Z_Filter(Z.Z_Derivative(Gyro), 0.8)
 D_Goal = Z.Z_Derivative(Goal)
 
-Orientation = Z.Z_Filter(Z.Z_Sensor(loc.get_teta), 0.4)
+Orientation = Z.Z_Filter(Z.Z_Sensor(loc.get_teta), 0.8)
+D_Orientation = Z.Z_Filter(Z.Z_Derivative(Orientation), 0.8)
+Teta_Goal = Z.Z_Constant(0.0)
+D_Teta_Goal = Z.Z_Filter(Z.Z_Derivative(Teta_Goal), 0.5)
 
-Motor =  Z.Z_PID(kva, kpa, tpa, kda, sata, Goal, Gyro, D_Goal, D_Gyro)
+Dir   = Z.Z_PID(kvo, kpo, tpo, kdo, sato, Teta_Goal, Orientation, D_Teta_Goal, D_Orientation)
+Motor = Z.Z_PID(kva, kpa, tpa, kda, sata, Goal, Gyro, D_Goal, D_Gyro)
 
-for i in range(1, 10000, 1):
+for i in range(1, 1000, 1):
 	loc.update()
+	Teta_Goal.set_val( 0.)
 	#print(Gyro.get_val(), "\t", Goal.get_val(), "\t", Motor.get_val())
+	d = Dir.get_val()
 	m = Motor.get_val()
-	if m >0:
-		m += 3
-	else:
-		m -= 3
-	pwmMotors.set_speed(m,m)
+	pwmMotors.set_speed(m-d,m+d)
 	#time.sleep(0.01)
 	Z.Z_Index += 1
 
 import RPi.GPIO as GPIO
 GPIO.cleanup()
-
+print("erreurs de localisations : ", loc.pos_errors)
