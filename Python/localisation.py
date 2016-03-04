f35071e38ad2c9e8db4c86059ebee8af6c365b67
openCrapal <ulysse.delplanque@gmail.com>
2016-03-04 01:29:17 +0100

    now using threading

--- Python/localisation.py
+++ Python/localisation.py
@@ -1,76 +1,52 @@
 #!/usr/bin/python3.4
 # -*-coding:utf-8 -*
 
+from constants import *
 import RPi.GPIO as GPIO
 import math
 from time import time, sleep
+from threading import Thread
+from automation import Z_Filter
 
 #GPIO.cleanup()
 GPIO.setmode(GPIO.BCM) # use hardware pin numbers
 GPIO.setwarnings(True)
 
-#Pins I use
-pinLeftA = 6
-pinLeftB = 13
-pinRightA= 19
-pinRightB= 26
-
-#Global localisition variables
-pos_x = 0.0
-pos_y = 0.0
-pos_teta = 0.0    #orientation in rads
-pos_speed = 0.0   #axial speed
-pos_way = 0.0
-
-pos_errors = 0
-timer = time()
-
-# physical dimentions of the vehicule
-dxLeft = 0.042*3.141/48
-dxRight= 0.042*3.141/48
-wheels_width = 0.128   # distance beetwin wheels
-
 class Encoder():
-	# This class could be use by itself, in case you have only one qudrature ecoder
-	# The point is to minimise calculation wihtin the interruptions i order not to miss any
+	# This class could be use by itself, in case you have only one quadrature encoder
+	# The point is to minimise calculation wihtin the interruptions in order not to miss any
 
 	def changeA(self, term):
-		newA = GPIO.input(self.pinA)
-		if self.A == newA:
-			self.erreur +=1
-		elif newA:  # rising edge on A
+		self.A = GPIO.input(self.pinA)
+		if self.A:  # risinging edge on A
 			if self.B:
 				self.count -= 1
 			else:
 				self.count += 1
-		else:
+		else:	# fallinging edge on A
 			if self.B:
 				self.count += 1
 			else:
 				self.count -= 1
-		self.A = newA
 
 	def changeB(self, term):
-		newB = GPIO.input(self.pinB)
-		if newB == self.B:
-			self.erreur += 1
-		elif newB:  # rising edge on B
+		self.B = GPIO.input(self.pinB)
+		if self.B:  # risinging edge on B
 			if self.A:
 				self.count += 1
 			else:
 				self.count -= 1
-		else:
+		else:	# falling edge on B
 			if self.A:
 				self.count -= 1
 			else:
 				self.count += 1
-		self.B = newB
 
 	def __init__(self, pinA, pinB):
 		self.pinA = pinA
 		self.pinB = pinB
 		self.count = 0
-		self.erreur= 0
+		self.erreurs= 0
 		GPIO.setup(pinA, GPIO.IN, pull_up_down=GPIO.PUD_UP) # pull_up_down=GPIO.PUD_UP
 		GPIO.setup(pinB, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 		self.A = GPIO.input(pinA)
@@ -92,6 +68,8 @@ class Encoder():
 				pass
 			else:
 				done = True
+		self.A = GPIO.input(pinA)
+		self.B = GPIO.input(pinB)
 
 	# this is the getter you wanna use for sensor displacement
 	def get_count(self):
@@ -99,84 +77,146 @@ class Encoder():
 		self.count = 0
 		return c
 
-# Declaration of the two object "Encoders" used of localisation
-#Enc_Left = Encoder(pinLeftA, pinLeftB)
-Enc_Right= Encoder(pinRightA,pinRightB)
-Enc_Left = Encoder(pinLeftA, pinLeftB)
+# Create an item of the loc class to get track of you position
+# The run() method must be called frequently
+class loc(Thread):
+	def __init__(self):
+		Thread.__init__(self)
+		self.left_speed = self.right_speed = self.pos_speed = 0.0
+		self.pos_x = self.pos_y = self.pos_teta = self.pos_way = 0.0
+		self.cycle_time = 0.005
+		self.t_filter = 0.01
+		self.NotDone = False
 
-# I must have done too much c++, there must be a more pythonic way to do that
-# This is for compability with my "automation" module
-def get_teta():
-	# update()
-	return (pos_teta)
+	def run(self):
+		self.NotDone = True
+		Enc_Right= Encoder(pinRightA,pinRightB)
+		Enc_Left = Encoder(pinLeftA, pinLeftB)
+		count_l = count_r = 0
+		DxLeft = dxLeft
+		DxRight= dxRight
+		DtetaLeft = math.atan(dxLeft /(2.0*wheels_width))
+		DtetaRight= math.atan(dxRight/(2.0*wheels_width))
+		t_old = time()
 
-def get_speed():
-	return (pos_speed)
+		while(self.NotDone):
+			count_l = - Enc_Left.get_count()
+			count_r =  Enc_Right.get_count()
+			#print(self.count_l, ";\t", self.count_r)
+			t = time()
+			#self.r += self.count_r
+			#self.l += self.count_l
+			dt = t - t_old
+			ls = DxLeft * count_l / dt
+			rs = DxRight* count_r / dt
 
-def get_x():
-	return(pos_x)
+			self.left_speed = (self.left_speed * self.t_filter + dt * ls ) / ( dt + self.t_filter)
+			self.right_speed = (self.right_speed* self.t_filter + dt * rs ) / ( dt + self.t_filter)
 
-def get_y():
-	return(pos_y)
+			t_old = t
 
-def get_way():
-	return(pos_way)
+			if (count_l != 0 or count_r != 0):
+				d_way = (count_l * DxLeft + count_r * DxRight) / 2.0 		# absolut deplacement step
+				self.pos_way += d_way						# "Abcisse curviligne" position on the roboter's trajectorie
+				half_d_teta  = - count_l * DtetaLeft + count_r * DtetaRight	# half rotation step
+				local_teta = self.pos_teta + half_d_teta			# local variable to store absolut orientation
+				self.pos_teta = local_teta + half_d_teta
+				self.pos_x += d_way * math.cos(local_teta)			# Note that the position is updated this only half of 
+				self.pos_y += d_way * math.sin(local_teta)			# ...the rotation step
+			
+				#self.pos_errors = self.Enc_Right.erreur + self.Enc_Left.erreur
 
-# To save time during interruptions, the "complex" trigonometric calculation is
-# done only when the user needs it
-# if you don't call it often enought, you might have some big localisation errors
-# Could be triggerd by a timer interrupt
-r = l = 0
-def update():
-	#GPIO.setmode(GPIO.BCM)
-	global timer
-	t = time()
-	global r, l
-	count_l = Enc_Left.get_count()
-	count_r = - Enc_Right.get_count()
-	r += count_r
-	l += count_l
-	if (count_l or count_r):
-		global pos_x, pos_y, pos_teta, pos_speed, pos_way
-		d_way = (count_l * dxLeft + count_r * dxRight) / 2.0
-		pos_way += d_way
-		pos_speed = d_way / ( t - timer )
-		half_d_teta  = count_l * math.atan(dxLeft/2.0/wheels_width) - count_r * math.atan(dxRight/2.0/wheels_width)
-		pos_teta -= half_d_teta
-		pos_x += d_way * math.cos(pos_teta)
-		pos_y += d_way * math.sin(pos_teta)
-		pos_teta -= half_d_teta
-	else:
-		pos_speed /= 2.0
-	timer = t
-	#pos_errors = Enc_Right.erreur + Enc_Left.erreur
+			t_sleep = self.cycle_time - time()  + t
+			if t_sleep < 0:
+				print ("Tread location : cycle time too short")
+				t_sleep = 0.001
+			sleep(t_sleep)
+		# End_While
+	# End_run
+
+
+	def get_teta(self):
+		return (self.pos_teta)
+
+	def get_speed(self):
+		return ((self.right_speed + self.left_speed) / 2.0)
+
+	def get_left_speed(self):
+		return (self.left_speed)
+
+	def get_right_speed(self):
+		return (self.right_speed)
+
+	def get_x(self):
+		return(self.pos_x)
+
+	def get_y(self):
+		return(self.pos_y)
+
+	def get_way(self):
+		return(self.pos_way)
+
+	def finish(self):
+		self.__init__()
+
+#End class loc
+
+
 
 if __name__ == "__main__":
-	import sys
-	import signal
-	import pwmMotors
-	pwmMotors.set_speed(-50.0,-50.0)
+	from i2cDev import i2c_devices 
+	i2cDevs = i2c_devices()
+	i2cDevs.set_speed(0,0)
+	MyLoc = loc()
+	MyLoc.start()
 
-	def fermer_pgrm(signal, frame):
-		print("fermer proprement")
-		GPIO.cleanup()
-		sys.exit(0)
+	def test_kv(Motors):
+		def test_value(value_pwm):
+			i2cDevs.set_speed(value_pwm, value_pwm)
+			sleep(0.01)
+			t = time()
+			sum_l = sum_r = 0.0
+			n = 0
+			while (time()-t < 0.001):
+				n+=1
+				sum_l += MyLoc.get_left_speed()
+				sum_r += MyLoc.get_right_speed()
+				sleep(0.001)
+			sum_r /= n
+			sum_l /= n
+			print (value_pwm, ";\t", sum_l, ",\t", sum_r, ";\t", MyLoc.get_speed())
+			
+		for i in range(0, 90, 1):
+			test_value(i)
+		for i in range(90, -90 , -1):
+			test_value(i)
+		for i in range(-90, 0, 1):
+			test_value(i)
 
-	signal.signal(signal.SIGINT, fermer_pgrm)
 	mla = mra = mlb = mrb = 0.0
 	x = 0
 	print ("pos_x \t pos_Y \t pos_teta \t pos_speed")
-#	while(not (x == 'q' or x == 'Q')):
-	while(x<400):
-		update()
-#		print( "l: ", l, "\tr: ", r)
-		print (pos_x,"\t", pos_y,"\t", pos_teta,"\t", pos_speed)
-		#mla = 0.95 * mla + 0.05 * GPIO.input(pinLeftA)
-		#mra = 0.95 * mra + 0.05 * GPIO.input(pinRightA)
-		#mlb = 0.95 * mlb + 0.05 * GPIO.input(pinLeftB)
-		#mrb = 0.95 * mrb + 0.05 * GPIO.input(pinRightB)
-		#print(mla,"\t",  mlb,"\t", mra,"\t", mrb) 
+	t = timer = time()
+	while(t - timer < 8):
+		t = time()
+#		i2cDevs.set_speed(0, 0)
+		print (MyLoc.get_x(),"\t", MyLoc.get_y(),"\t", MyLoc.get_teta(),"\t", MyLoc.get_speed())
+		#mla = 0.99 * mla + 0.01 * GPIO.input(pinLeftA)
+		#mra = 0.99 * mra + 0.01 * GPIO.input(pinRightA)
+		#mlb = 0.99 * mlb + 0.01 * GPIO.input(pinLeftB)
+		#mrb = 0.99 * mrb + 0.01 * GPIO.input(pinRightB)
+#		print(mla,"\t",  mlb,"\t", mra,"\t", mrb) 
 #		print(GPIO.input(pinLeftA),GPIO.input(pinLeftB),GPIO.input(pinRightA),GPIO.input(pinRightB))
-#		x = keyboard.read(100, timeout = 0)
-#		sleep(0.01)
+		sleep(0.1)
 		x += 1
+
+#	test_kv(pwmMotors)
+
+	while math.copysign(x, 1) > 10:
+		i2cDevs.set_speed(0, x)
+		x -= math.copysign( 1, x)
+		sleep(0.01)
+	i2cDevs.set_speed(0,0)
+	i2cDevs.finish()
+	MyLoc.finish()
+	
